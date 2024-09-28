40c26379e36ce361fd2c41a8b1e64e586118bd2d
Asimovldn <asimovldn@gmail.com>
2024-09-28 13:06:23 -0300

    Adicionou o @teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testes/TesteLinearSlideIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testes/TesteLinearSlideIntake.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.Testes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Auxiliar.Intake;
 
+@TeleOp
 public class TesteLinearSlideIntake extends LinearOpMode
 {
     @Override

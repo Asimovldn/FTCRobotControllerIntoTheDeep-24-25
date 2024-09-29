ff5d261f174798d06e0cfa625501bff7ae5428c2
Asimovldn <asimovldn@gmail.com>
2024-09-29 16:51:22 -0300

    Adição dos códigos de FeedForward e PID

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveAndLift/Controls/FeedForward.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.DriveAndLift.Controls;
+
+public class FeedForward
+{
+    double ka;
+    double kv;
+    double ks;
+    double kg;
+
+    public FeedForward(double kv, double ka, double ks)
+    {
+        this.kv = kv;
+        this.ka = ka;
+        this.ks = ks;
+    }
+
+    public FeedForward(double kv, double ka, double ks, double kg)
+    {
+        this.kv = kv;
+        this.ka = ka;
+        this.ks = ks;
+        this.kg = kg;
+    }
+
+    public double calculate(double vSetpoint, double aSetPoint)
+    {
+        return ks + vSetpoint * kv + aSetPoint * ka;
+    }
+
+    public double calculate(double vSetpoint, double aSetpoint, double gFactor)
+    {
+        return ks + vSetpoint * kv + aSetpoint * ka + gFactor * kg;
+    }
+}

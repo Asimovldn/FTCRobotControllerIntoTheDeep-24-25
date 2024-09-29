90d2635c09c5f4edc1b1333fda48e58e98566f2b
Asimovldn <asimovldn@gmail.com>
2024-09-29 00:46:41 -0300

    Adição do código para movimentar o slide e servo
    Adiciona os métodos para movimentar os linear slides do outtake por potencia e os servos por posição

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auxiliar/Outtake.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.Auxiliar;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Outtake
+{
+    DcMotorEx leftSlide;
+    DcMotorEx rightSlide;
+
+    Servo leftServo;
+    Servo rightServo;
+
+    double inLeftPosition = 0.0;
+    double inRightPosition = 0.0;
+
+    double outLeftPosition = 0.0;
+    double outRightPosition = 0.0;
+
+    public enum OuttakePosition
+    {
+        OUT, IN
+    };
+
+    public Outtake(HardwareMap hardwareMap)
+    {
+        leftSlide = hardwareMap.get(DcMotorEx.class, "left_outtake_slide");
+        rightSlide = hardwareMap.get(DcMotorEx.class, "right_outtake_slide");
+
+        leftSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftServo = hardwareMap.get(Servo.class, "left_outtake_servo");
+        rightServo = hardwareMap.get(Servo.class, "right_outtake_servo");
+    }
+
+    public void Move(double power)
+    {
+        leftSlide.setPower(power);
+        rightSlide.setPower(power);
+    }
+
+    public void MoveServo(OuttakePosition outtakePosition)
+    {
+        switch (outtakePosition)
+        {
+            case IN:
+                leftServo.setPosition(inLeftPosition);
+                rightServo.setPosition(inRightPosition);
+                break;
+
+            case OUT:
+                leftServo.setPosition(outLeftPosition);
+                rightServo.setPosition(outRightPosition);
+                break;
+        }
+    }
+}
